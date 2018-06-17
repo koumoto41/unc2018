ad2d9808f6be8c57dfe07b14623a9831ce033b8a
Hiroyuki Komoto <koumoto41@gmail.com>
2018-06-17 14:44:43 +0900

    設定ファイル変更

--- unc/wp-config.php
+++ unc/wp-config.php
@@ -22,38 +22,34 @@ define('W3TC_EDGE_MODE', true); // Added by W3 Total Cache
 // (http://wpdocs.sourceforge.jp/Codex:%E8%AB%87%E8%A9%B1%E5%AE%A4 参照)
 // を使用し、必ず UTF-8 の BOM なし (UTF-8N) で保存してください。
 
-if (isset( $_SERVER['SERVER_NAME'] ) == true) {
-	if ($_SERVER['SERVER_NAME'] == 'natural-camp.test') {
-		define('CONST_SERVER_MODE', false);
-	} else {
-		define('CONST_SERVER_MODE', true);
-	}
+if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'natural-camp.jp') {
+	define('CONST_SERVER_MODE', true);
 } else {
-	define( 'CONST_SERVER_MODE', true);
+	define('CONST_SERVER_MODE', false);
 }
 
 
 // ** MySQL 設定 ** //
-if (CONST_SERVER_MODE === true) {
-	define('DB_NAME',     'LAA0038227-unc2017');
-	define('DB_USER',     'LAA0038227');
+if (CONST_SERVER_MODE) {
+	define('DB_NAME', 'LAA0038227-unc2017');
+	define('DB_USER', 'LAA0038227');
 	define('DB_PASSWORD', 'qfwcqkow');
-	define('DB_HOST',     'mysql123.phy.lolipop.lan');
-	define('DB_CHARSET',  'utf8');
-	define('DB_COLLATE',  '');
+	define('DB_HOST', 'mysql123.phy.lolipop.lan');
+	define('DB_CHARSET', 'utf8');
+	define('DB_COLLATE', '');
 
-	define('WP_DEBUG',    false);
+	define('WP_DEBUG', false);
 } else {
-	define('DB_NAME',     'unc2018');
-	define('DB_USER',     'root');
+	define('DB_NAME', 'unc2018');
+	define('DB_USER', 'root');
 	define('DB_PASSWORD', 'root');
-	define('DB_HOST',     'localhost');
-	define('DB_CHARSET',  'utf8');
-	define('DB_COLLATE',  '');
+	define('DB_HOST', 'localhost');
+	define('DB_CHARSET', 'utf8');
+	define('DB_COLLATE', '');
 
-	define('WP_DEBUG',    true);
+	define('WP_DEBUG', true);
 	define('FS_METHOD', 'direct');
-    define( 'WP_MEMORY_LIMIT', '512M' );
+	define('WP_MEMORY_LIMIT', '1024M');
 }
 
 
@@ -66,14 +62,14 @@ if (CONST_SERVER_MODE === true) {
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'ndug*K@HY5iy4c.OjK8,L(y91W4y2-I?ono}h;ED4c7&mc*VkK.6eUSD^*YXm5o|');
-define('SECURE_AUTH_KEY',  'z|q>]5[>9LdIsQV;v`r^{E(m17:=R1xCYfAcA(t+Wf>UyuLD3/Br3>x*1ugoA wt');
-define('LOGGED_IN_KEY',    '84Ue`)u>}j%jDh+t~S_gy+BYN_(Ya[HPi$U;xDrj10S$:.3P)8vWdV4|>/hj~4CK');
-define('NONCE_KEY',        ' ^n@F].0t@/bC.>_f){_2ZsufdRi !4[!d9CCM1j@>^0vYv|4NqY#u3hZy`YYs-l');
-define('AUTH_SALT',        'na!L+*Ba$VB_aY=Xa@oqNbi4FMoTf{S?:]OVe.V|izwRyLNMDMvH0z3o%7]^ H<V');
+define('AUTH_KEY', 'ndug*K@HY5iy4c.OjK8,L(y91W4y2-I?ono}h;ED4c7&mc*VkK.6eUSD^*YXm5o|');
+define('SECURE_AUTH_KEY', 'z|q>]5[>9LdIsQV;v`r^{E(m17:=R1xCYfAcA(t+Wf>UyuLD3/Br3>x*1ugoA wt');
+define('LOGGED_IN_KEY', '84Ue`)u>}j%jDh+t~S_gy+BYN_(Ya[HPi$U;xDrj10S$:.3P)8vWdV4|>/hj~4CK');
+define('NONCE_KEY', ' ^n@F].0t@/bC.>_f){_2ZsufdRi !4[!d9CCM1j@>^0vYv|4NqY#u3hZy`YYs-l');
+define('AUTH_SALT', 'na!L+*Ba$VB_aY=Xa@oqNbi4FMoTf{S?:]OVe.V|izwRyLNMDMvH0z3o%7]^ H<V');
 define('SECURE_AUTH_SALT', '1hc_|V(1!0nFgmVqblmG+G<%UoG)/}%Iw)T}L!Y1:x(IJ:c>~S+A3XR0dR$>8F-z');
-define('LOGGED_IN_SALT',   '5gQGgmrpj?qei^Lz{<t$aTvRpc]oLWgE~=8[8+Rg8#?p@.9FnIS 29EUZF?djnui');
-define('NONCE_SALT',       ',;pPr5MOk}jLyjE{_YL-JpW.sdiz^$wzDSweA0d]YKQaKIi,J uH8mM8hRP.ZZN|');
+define('LOGGED_IN_SALT', '5gQGgmrpj?qei^Lz{<t$aTvRpc]oLWgE~=8[8+Rg8#?p@.9FnIS 29EUZF?djnui');
+define('NONCE_SALT', ',;pPr5MOk}jLyjE{_YL-JpW.sdiz^$wzDSweA0d]YKQaKIi,J uH8mM8hRP.ZZN|');
 
 /**#@-*/
 
@@ -83,7 +79,7 @@ define('NONCE_SALT',       ',;pPr5MOk}jLyjE{_YL-JpW.sdiz^$wzDSweA0d]YKQaKIi,J uH
  * それぞれにユニーク (一意) な接頭辞を与えることで一つのデータベースに複数の WordPress を
  * インストールすることができます。半角英数字と下線のみを使用してください。
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
 /**
  * ローカル言語 - このパッケージでは初期値として 'ja' (日本語 UTF-8) が設定されています。
@@ -93,15 +89,15 @@ $table_prefix  = 'wp_';
  * wp-content/languages にインストールし WPLANG を 'de_DE' に設定することでドイツ語がサポートされます。
  */
 define('WPLANG', 'ja');
-define('FS_METHOD','direct');
+define('FS_METHOD', 'direct');
 define('AUTOMATIC_UPDATER_DISABLED', true);
-define('CONCATENATE_SCRIPTS', false );
+define('CONCATENATE_SCRIPTS', false);
 
 
 /* 編集が必要なのはここまでです ! WordPress でブログをお楽しみください。 */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
+if (!defined('ABSPATH'))
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
@@ -111,13 +107,13 @@ require_once(ABSPATH . 'wp-settings.php');
 // URL,パス
 // +++++++++++++++++++++++++++++
 // テーマディレクトリパス
-define('DIR_PATH',  ABSPATH.'wp-content/themes/unc/');
+define('DIR_PATH', ABSPATH . 'wp-content/themes/unc/');
 // URL
 define('DOMAIN_URL', 'http://natural-camp.test');
 // サイトURL
-define('SITE_URL',  get_bloginfo('url'));
+define('SITE_URL', get_bloginfo('url'));
 // ワードプレスURL
-define('WP_URL',    get_bloginfo('wpurl'));
+define('WP_URL', get_bloginfo('wpurl'));
 // ワードプレステーマURL
 define('THEME_URL', get_bloginfo('template_directory'));
 
@@ -125,9 +121,9 @@ define('THEME_URL', get_bloginfo('template_directory'));
 // デフォルトキーワード
 // +++++++++++++++++++++++++++++
 // タイトルデフォルト
-define('DEF_TITLE',      get_bloginfo('name'));
+define('DEF_TITLE', get_bloginfo('name'));
 // キーワードデフォルト
-define('DEF_KEYWORDS',   '牛窓ナチュラルキャンプ,牛窓ナチュラルキャンプ2017,うしまど,岡山県,瀬戸内市,黒島,無人島');
+define('DEF_KEYWORDS', '牛窓ナチュラルキャンプ,牛窓ナチュラルキャンプ2018,うしまど,岡山県,瀬戸内市,黒島,無人島');
 // ディスクリプションデフォルト
 define('DEF_DESCRIPTION', get_bloginfo('description'));
 
@@ -135,4 +131,9 @@ define('DEF_DESCRIPTION', get_bloginfo('description'));
 define('DIFF_DAY', 14);
 
 // LIVE STAGE配列
-$aryCommonStage = array('treestage'=>'TreeStage', 'littleforest'=>'LittleForest', 'littleiviza'=>'LittleIviza', 'unkown'=>'Unkown');
+$aryCommonStage = array(
+	'treestage'    => 'TreeStage',
+	'littleforest' => 'LittleForest',
+	'littleiviza'  => 'LittleIviza',
+	'unkown'       => 'Unkown'
+);
